666979d4116e610537490230f78a36c8c529a62d
Rich <piemonkey@gmail.com>
2016-12-31 18:06:13 +0000

    Add card-badges function to collect card numbers

--- js/index.js
+++ js/index.js
@@ -1,2 +1,31 @@
+'use strict'
+
+var cards = {}
+
 TrelloPowerUp.initialize({
+  'card-badges': function(t) {
+    var cardId = ''
+    t.card('id')
+      .get('id')
+      .then(function(id) {
+        cardId = id
+        cards[id] = {t: t}
+        t.card('url')
+          .get('url')
+          .then(function(url) {
+            cards[id].number = url.slice(url.lastIndexOf('/') + 1, url.indexOf('-'))
+          })
+      })
+
+    return [{
+      dynamic: function() {
+        return {
+          text: cards[cardId].number,
+          icon: './images/logo.svg',
+          color: 'green',
+          refresh: 20,
+        }
+      },
+    }]
+  },
 })
