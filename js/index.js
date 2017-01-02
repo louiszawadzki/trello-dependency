5f9f20eae020257efaececc5255019935300fc0b
Rich <piemonkey@gmail.com>
2017-01-02 21:01:05 +0000

    Build up map of cards to children

--- js/index.js
+++ js/index.js
@@ -1,22 +1,45 @@
 'use strict'
 
+var cardLabelRegex = /^#? ?(\d+)$/
+
 var cards = {}
+var children = {}
 
 TrelloPowerUp.initialize({
   'card-badges': function(t) {
     var cardId = ''
-    return t.card('id', 'url')
+    return t.card('id', 'url', 'labels')
       .then(function(card) {
         cardId = card.id
         if (!cards[cardId]) {
           cards[cardId] = {
             t: t,
             number: card.url.slice(card.url.lastIndexOf('/') + 1, card.url.indexOf('-')),
+            labels: [],
           }
         }
+        var oldLabels = cards[cardId].labels
+        var currentLabels = card.labels.map(function(label) {return label.name})
+        currentLabels
+          .filter(function(label) {return oldLabels.indexOf(label) === -1})
+          .map(function(label) {return cardLabelRegex.exec(label)})
+          .forEach(function(match) {
+            if (match && match[1]) {
+              children[match[1]] = children[match[1]] ? children[match[1]].concat([cardId]) : [cardId]
+            }
+          })
+        oldLabels
+          .filter(function(label) {return currentLabels.indexOf(label) === -1})
+          .map(function(label) {return cardLabelRegex.exec(label)})
+          .forEach(function(match) {
+            if (match && match[1]) {
+              children[match[1]] = children[match[1]].filter(function(id) {return id !== cardId})
+            }
+          })
+        cards[cardId].labels = currentLabels
 
         return [{
-          text: cards[cardId].number,
+          text: children[cards[cardId].number] ? '- / ' + children[cards[cardId].number].length: cards[cardId].number,
           icon: './images/logo.svg',
           color: 'green',
         }]
